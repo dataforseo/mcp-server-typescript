856931f1e6e8ded54169d6647154fe9d97db42d1
mykyta.biliaiev <mykyta.biliaev@softoria.com>
2025-11-23 17:12:30 +0200

    add chat_gpt scraper

---
+++ src/core/modules/ai-optimization/tools/llms/chat-gpt/cha-gpt-locations.ts
@@ -0,0 +1,52 @@
+import { z } from 'zod';
+import { DataForSEOClient } from '../../../../../client/dataforseo.client.js';
+import { BaseTool } from '../../../../base.tool.js';
+
+export class AiOptimizationChatGptLocationsTool extends BaseTool {
+
+    constructor(dataForSEOClient: DataForSEOClient) {
+        super(dataForSEOClient);
+    }
+
+    protected supportOnlyFullResponse(): boolean {
+        return true;
+    }
+
+    getName(): string {
+        return "ai_optimization_chat_gpt_locations";
+    }
+
+    getDescription(): string {
+        return "Utility tool for ai_optimization_chat_gpt_scraper to get list of available locations";
+    }
+
+    getParams(): z.ZodRawShape {
+        return {
+            country_iso_code: z.string().optional().describe("ISO 3166-1 alpha-2 country code, for example: US, GB, MT"),
+            location_type: z.string().optional().describe("Type of location. Possible variants: 'TV Region','Postal Code','Neighborhood','Governorate','National Park','Quarter','Canton','Airport','Okrug','Prefecture','City','Country','Province','Barrio','Sub-District','Congressional District','Municipality District','district','DMA Region','Union Territory','Territory','Colloquial Area','Autonomous Community','Borough','County','State','District','City Region','Commune','Region','Department','Division','Sub-Ward','Municipality','University'"),
+            location_name: z.string().optional().describe("Name of location or it`s part.")
+        };
+    }
+
+    async handle(params: any): Promise<any> {
+        try {
+
+            let payload: Record<string, unknown> = {};
+            if (params.country_iso_code) {
+                payload['country_iso_code'] = params.country_iso_code;
+            }
+            if (params.location_type) {
+                payload['location_type'] = params.location_type;
+            }
+            if (params.location_name) {
+                payload['location_name'] = params.location_name;
+            }
+
+            let method = payload && Object.keys(payload).length > 0 ? 'POST' : 'GET';
+            const response = await this.dataForSEOClient.makeRequest(`/v3/ai_optimization/chat_gpt/llm_scraper/locations`, method, method === 'POST' ? [payload] : null) as any;
+            return this.formatResponse(response['items']);
+        } catch (error) {
+            return this.formatErrorResponse(error);
+        }
+    }
+}
